2fbbd722483b71a8b2bd19c39191c414c7299161
unknown <carlson.king@binus.ac.id>
2023-05-01 21:02:37 +0700

    updatedphpfile

--- index.php
+++ index.php
@@ -20,6 +20,9 @@
     # Mendapatkan hasil dari variable sql diatas.
     $res = $conn->query($sql);
 
+    # Membuat variable untuk menghitung jumlah data yang ada
+    $total = 0;
+
     # Validasi yang dilakukan sesuai dengan data yang didapatkan variable res.
     if($res->num_rows > 0)
     {
@@ -27,7 +30,10 @@
         while($row = $res->fetch_assoc())
         {
             echo "ID: ". $row["ID"]. " | Nama: ". $row["Nama"]. " | Alamat: ". $row["Alamat"]. " | Jabatan: ". $row["Jabatan"]. "<br>";
+            $total = $total++;
         }
+        # Menampilkan jumlah data yang ada
+        echo "Total data: ". $total;
     }
     else
     {
